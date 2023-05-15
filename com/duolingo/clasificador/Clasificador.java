8a9aaa553532719233eb8ceeefc76cbc76b5adaa
JereDAM <usr3593@salesianos-lacuesta.net>
2023-05-15 09:50:20 +0100

    Refactor(Clasificador): Se refactorizó el metodo añadir palabra.

--- com/duolingo/clasificador/Clasificador.java
+++ com/duolingo/clasificador/Clasificador.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Clasificador {
 
     private Map<String, Set<String>> listaGeneral = new HashMap<>();
+    private Set<String> primeraLetra = new HashSet<>();
     
     private String obtenerPrimeraLetra(String palabra){ 
         return palabra.substring(0,1).toLowerCase();
@@ -17,7 +18,7 @@ public class Clasificador {
         if(listaGeneral.containsKey(obtenerPrimeraLetra(nuevaPalabra))){    
             // Busco en el Map si existe un Set para esa inicial
             //Si existe el Set, lo obtengo, añado la palabra y actualizo el mapa con un put
-            Set<String> primeraLetra = listaGeneral.get(obtenerPrimeraLetra(nuevaPalabra));;
+            primeraLetra = listaGeneral.get(obtenerPrimeraLetra(nuevaPalabra));;
             primeraLetra.add(nuevaPalabra);
             listaGeneral.put(obtenerPrimeraLetra(nuevaPalabra), primeraLetra);
             System.out.println(listaGeneral);
@@ -28,6 +29,5 @@ public class Clasificador {
             listaGeneral.put(obtenerPrimeraLetra(nuevaPalabra), nuevaLetra);
             System.out.println(listaGeneral);
         }
-        
     }
 }
--- src/App.java
+++ src/App.java
@@ -2,21 +2,9 @@ package src;
 
 import java.util.Scanner;
 
-import com.duolingo.clasificador.Clasificador;
-
 public class App {
     public static void main(String[] args){
 
-        Scanner teclado = new Scanner(System.in);      
-
-        Clasificador micClasificador = new Clasificador();
-        
-        
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Introduce un nombre");
-            String nombre = teclado.nextLine();
-            micClasificador.anadirPalabra(nombre);
-        }
 
 
     }
