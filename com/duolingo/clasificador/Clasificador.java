6cedc70a98f417e788808e9b994d2bc315f9f96b
JereDAM <usr3593@salesianos-lacuesta.net>
2023-05-16 18:34:21 +0200

    Feat(Clasificador): Se añadio un metodo mostrar letras.

--- com/duolingo/clasificador/Clasificador.java
+++ com/duolingo/clasificador/Clasificador.java
@@ -18,25 +18,33 @@ public class Clasificador {
         if(listaGeneral.containsKey(obtenerPrimeraLetra(nuevaPalabra))){    
             // Busco en el Map si existe un Set para esa inicial
             //Si existe el Set, lo obtengo, añado la palabra y actualizo el mapa con un put
-            primeraLetra = listaGeneral.get(obtenerPrimeraLetra(nuevaPalabra));;
+            primeraLetra = listaGeneral.get(obtenerPrimeraLetra(nuevaPalabra));
             primeraLetra.add(nuevaPalabra);
             listaGeneral.put(obtenerPrimeraLetra(nuevaPalabra), primeraLetra);
             System.out.println("Palabra añadida a lista");
+            System.out.println(listaGeneral);
         }else{
             // Si no existe el Set, creo un Set nuevo, y lo guardo en el Map para esa letra
             Set<String> nuevaLetra = new HashSet<>();
             nuevaLetra.add(nuevaPalabra.toLowerCase());
             listaGeneral.put(obtenerPrimeraLetra(nuevaPalabra), nuevaLetra);
             System.out.println("Lista creada y palabra nueva añadida");
+            System.out.println(listaGeneral);
         }
     }
 
     public void eliminarPalabra(String palabraEliminar){
         if(listaGeneral.containsKey(obtenerPrimeraLetra(palabraEliminar))){ // Compruebo que existe la colección y luego elimino la palabra de la coleccion
+            primeraLetra = listaGeneral.get(obtenerPrimeraLetra(palabraEliminar));
             primeraLetra.remove(palabraEliminar);
             System.out.println("Palabra encontrada y eliminada");
+            System.out.println(listaGeneral);
+            if(primeraLetra.isEmpty()){
+                listaGeneral.remove(obtenerPrimeraLetra(palabraEliminar));
+            }
         }else{
             System.out.println("la palabra que desea eliminar no existe");
+            System.out.println(listaGeneral);
         }
     }
 
@@ -47,18 +55,30 @@ public class Clasificador {
                 System.out.println("Palabra encontrada");
             }else{
                 System.out.println("La palabra no existe");
+                System.out.println(listaGeneral);
             }
         }else{
             System.out.println("La palabra no existe");
+            System.out.println(listaGeneral);
         }
     }
 
     public void mostrarPalabrasInicial(String letra){
 
-        if(listaGeneral.containsKey(letra)){
+        primeraLetra = listaGeneral.get(obtenerPrimeraLetra(letra));
+
+        if(listaGeneral.containsKey(obtenerPrimeraLetra(letra))){
             System.out.println(primeraLetra);
         }else{
             System.out.println("La letra no está en la lista");
+        }
+    }
+
+    public void mostrarLetras(){
+        if(listaGeneral.isEmpty()){
+            System.out.println("El diccionario está vacío");
+        }else{
+            System.out.println(listaGeneral.keySet());
         }
     }
 }
